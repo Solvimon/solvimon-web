02d4c3a548c29d8b0e5eb73a7b9f458f5433d6b4
Kasper van Veen <kasper.vanveen@solvimon.com>
2025-09-17 12:23:42 +0000

    DD-2333 chore(Gitlab pipeline): seperate prettier from eslint
    Verify before merging: ensure compliance with our [Secure Engineering Principles](https://solvimon.getoutline.com/s/21fbef7a-ff0d-4b88-ae5c-946546875f33) and [Definition of Done](https://solvimon.getoutline.com/doc/definition-of-done-90fb00NtJw).
    
    Closes DD-2333

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,11 +10,11 @@ module.exports = {
         'plugin:vue/vue3-essential',
         'eslint:recommended',
         '@vue/eslint-config-typescript',
-        'prettier',
         'plugin:import/recommended',
         '@solvimon/eslint-config',
+        'prettier',
     ],
-    plugins: ['@html-eslint', 'unused-imports', 'prettier', 'no-relative-import-paths'],
+    plugins: ['@html-eslint', 'unused-imports', 'no-relative-import-paths'],
     parserOptions: {
         ecmaVersion: 'latest',
         parser: '@typescript-eslint/parser', // Specify TypeScript parser for `<script lang="ts">`

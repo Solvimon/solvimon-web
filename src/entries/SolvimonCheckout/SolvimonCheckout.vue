94f255425f7314c35d4193c8d0ecca13a66d7980
Leroy Doornebal <leroy.doornebal@solvimon.com>
2025-10-28 13:21:19 +0000

    DD-2431 feat(Portal; Checkout; Logging): Improve logging and email and country validation
    Verify before merging: ensure compliance with our [Secure Engineering Principles](https://solvimon.getoutline.com/s/21fbef7a-ff0d-4b88-ae5c-946546875f33) and [Definition of Done](https://solvimon.getoutline.com/doc/definition-of-done-90fb00NtJw).
    
    Closes DD-2431

--- src/entries/SolvimonCheckout/SolvimonCheckout.vue
+++ src/entries/SolvimonCheckout/SolvimonCheckout.vue
@@ -1,15 +1,41 @@
 <script setup lang="ts">
 import { isValidCountryCode } from '@solvimon/ui';
+import type { CountryCode } from '@solvimon/types';
 import type { SolvimonCheckoutEmits, SolvimonCheckoutProps } from './SolvimonCheckout.types';
 import Provider from '@/components/providers/Provider/Provider.vue';
 import Checkout from '@/views/Checkout/Checkout.vue';
 
 const props = defineProps<Partial<SolvimonCheckoutProps>>();
-defineEmits<SolvimonCheckoutEmits>();
+const emit = defineEmits<SolvimonCheckoutEmits>();
 
-if (props.countryCode && !isValidCountryCode(props.countryCode)) {
-    throw new Error(`invalid country code provided: "${props.countryCode}"`);
-}
+const getValidCountryCode = (countryCode: CountryCode | undefined) => {
+    if (countryCode) {
+        if (isValidCountryCode(countryCode)) {
+            return countryCode;
+        }
+
+        emit('error', new Error(`invalid country code provided: "${countryCode}"`));
+        return undefined;
+    }
+
+    return undefined;
+};
+
+const getValidEmail = (email: string | undefined) => {
+    if (email) {
+        if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+            return email;
+        }
+
+        emit('error', new Error(`invalid email provided: "${email}"`));
+        return undefined;
+    }
+
+    return undefined;
+};
+
+const validCountryCode = getValidCountryCode(props.countryCode);
+const validEmail = getValidEmail(props.email);
 </script>
 
 <template>
@@ -22,8 +48,8 @@ if (props.countryCode && !isValidCountryCode(props.countryCode)) {
     >
         <Checkout
             :avatar="avatar"
-            :email="email"
-            :country-code="countryCode"
+            :email="validEmail"
+            :country-code="validCountryCode"
             :enabled-pricing-ids="enabledPricingIds"
         >
             <template v-if="$slots['terms-and-conditions']" #terms-and-conditions

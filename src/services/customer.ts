08c4e09ee92645f311be75d781d90847d31bce84
nastia <nastia.piven@solvimon.com>
2026-05-06 14:44:40 +0200

    DD-2802: Fix composable

--- src/services/customer.ts
+++ src/services/customer.ts
@@ -2,10 +2,14 @@ import type { CheckTaxIDPayload, Customer, TaxIdValidationResult } from '@solvim
 import { createRequestService } from './requests';
 import { useConfig } from '@/components/providers/ConfigProvider/composables/useConfig';
 
+interface TaxIdValidationResponse {
+    data: TaxIdValidationResult[];
+}
+
 interface CustomerService {
     getCustomer: (customerId: Customer['id']) => Promise<Customer>;
     updateCustomer: (customerId: Customer['id'], payload: Partial<Customer>) => Promise<Customer>;
-    checkTaxID: (payload: CheckTaxIDPayload) => Promise<TaxIdValidationResult>;
+    checkTaxID: (payload: CheckTaxIDPayload) => Promise<TaxIdValidationResponse>;
 }
 
 export function createCustomerService(): CustomerService {
@@ -27,7 +31,7 @@ export function createCustomerService(): CustomerService {
     }
 
     function checkTaxID(payload: CheckTaxIDPayload) {
-        return request<TaxIdValidationResult>({
+        return request<TaxIdValidationResponse>({
             url: `${config.apiUrls.config}/portal/customers/validate-tax-id`,
             options: { method: 'POST' },
             data: payload,

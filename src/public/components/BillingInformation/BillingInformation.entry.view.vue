095568604951187e1db776797c8a606638e8a9da
leroy.doornebal_solvimon.com <leroy.doornebal@solvimon.com>
2026-05-07 14:56:47 +0200

    DD-2823 feat(billing information component): load initial data

--- src/public/components/BillingInformation/BillingInformation.entry.view.vue
+++ src/public/components/BillingInformation/BillingInformation.entry.view.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts">
 import type { SolvimonBillingInformationEntryProps } from './BillingInformation.entry.types';
 import { useCustomer } from '@/composables/useCustomer';
+import { useLoadInitialData } from '@/composables/useLoadInitialData';
 
 const props = defineProps<SolvimonBillingInformationEntryProps>();
 
 const customerId = props.portalObject.customer_id;
 
 const customer = useCustomer({ customerId });
+
+const { isLoading } = useLoadInitialData(customer.get.execute());
 </script>
 
 <template>
-    <slot name="default" :customer="customer" />
+    <slot name="default" :customer="customer" :is-loading="isLoading" />
 </template>

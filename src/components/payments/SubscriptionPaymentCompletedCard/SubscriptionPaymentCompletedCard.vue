c04d6b26f013611c6c1acf7d5916416949c6b83a
leroy.doornebal_solvimon.com <leroy.doornebal@solvimon.com>
2026-06-30 13:57:45 +0200

    feat(DD-2979): remove `continue to merchant` button

--- src/components/payments/SubscriptionPaymentCompletedCard/SubscriptionPaymentCompletedCard.vue
+++ src/components/payments/SubscriptionPaymentCompletedCard/SubscriptionPaymentCompletedCard.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { Typography, useIntl, Button } from '@solvimon/solvimon-ui';
+import { Typography, useIntl } from '@solvimon/solvimon-ui';
 import type { SubscriptionPaymentCompletedCardEmits } from './SubscriptionPaymentCompletedCard.types';
 import PaymentFeedbackCard from '@/components/payments/PaymentFeedbackCard/PaymentFeedbackCard.vue';
 
@@ -28,20 +28,5 @@ const { $t } = useIntl();
                 })
             }}
         </Typography>
-        <Button
-            color="gray"
-            variant="outline"
-            size="xs"
-            class="align-center"
-            @click="$emit('continue-to-merchant')"
-        >
-            {{
-                $t({
-                    defaultMessage: 'Continue to merchant',
-                    id: 'subscription_payment_completed_card.continue',
-                    description: 'The text for the continue button',
-                })
-            }}
-        </Button>
     </PaymentFeedbackCard>
 </template>

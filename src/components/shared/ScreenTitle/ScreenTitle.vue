9f6c1efea1d45534f44b89d573396e4a3b66e49e
leroy.doornebal_solvimon.com <leroy.doornebal@solvimon.com>
2026-07-03 11:23:25 +0200

    feat(DD-2979): add `ScreenTitle` component

---
+++ src/components/shared/ScreenTitle/ScreenTitle.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { Typography } from '@solvimon/solvimon-ui';
+import type { ScreenTitleProps } from './ScreenTitle.types';
+
+defineProps<ScreenTitleProps>();
+</script>
+
+<template>
+    <div class="grid gap-4 sm:grid-cols-1 md:grid-cols-2">
+        <div>
+            <Typography variant="heading-1">{{ title }}</Typography>
+            <Typography v-if="description" variant="body-sm" shade="lighter">{{
+                description
+            }}</Typography>
+        </div>
+        <div v-if="$slots.right"><slot name="right" /></div>
+    </div>
+</template>
